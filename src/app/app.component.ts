25af51b16adcb264b8e9b150d4de2e093bf22d85
ruston-RnY <rustonefendi493@gmail.com>
2021-11-23 11:50:31 +0700

    add aos animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-pedia';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      AOS.init({
+        once: true
+      });
+    }, 1000);
+  }
 }
